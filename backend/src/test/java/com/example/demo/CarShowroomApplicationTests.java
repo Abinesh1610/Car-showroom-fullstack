c6039fa9f9a4e534e9673118da08c6c360458468
Abinesh1610 <your.email@github.com>
2026-06-23 13:32:18 +0530

    Initial commit

--- backend/src/test/java/com/example/demo/CarShowroomApplicationTests.java
+++ backend/src/test/java/com/example/demo/CarShowroomApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+import com.example.carshowroom.CarShowroomApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CarShowroomApplication.class)
 class CarShowroomApplicationTests {
 
 	@Test
